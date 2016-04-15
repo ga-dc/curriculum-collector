b0f4cdb438576885fb57f6d49d0f3b70d2c971c4
Robert Thomas <robertakarobin@gmail.com>
2016-04-15 16:40:09 -0400

    Update RUN-ME.sh

--- RUN-ME.sh
+++ RUN-ME.sh
@@ -1,4 +1,4 @@
-source config.sh
+source .config.sh
 
 echo "Downloading all repos to '/$TARGET_FOLDER'. This may take a minute..."
 ruby .script.rb $INPUT_URL $TARGET_FOLDER $HTTPS_OR_SSH > .commands.sh
